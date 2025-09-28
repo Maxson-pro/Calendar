0279f66888c25beeef3be43263785c013add35bb
Maxson-pro <maxmeleshkin2012@gmail.com>
2025-09-28 09:36:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,22 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Day day = new Day(scanner);
-        Month month = new Month(scanner);
-        Year year = new Year(scanner);
+        Date date = new Date(scanner);
 
-        int daysInMonth = DaysInMonth.getDaysInMonth(month.getMonth(), year.getYear());
-        while (day.getDay() > daysInMonth) {
+        int daysInMonth = DaysInMonth.getDaysInMonth(date.getMonth(), date.getYear());
+        while (date.getDay() > daysInMonth) {
             System.out.println("Ошибка нету столько дней в месяце");
-            day = new Day(scanner);
+            date = new Date(scanner);
         }
 
-        int dayOfWeek = DayOfWeekCalculator.calculateDayOfWeek(day.getDay(), month.getMonth(), year.getYear());
+        int dayOfWeek = DayOfWeekCalculator.calculateDayOfWeek(date.getDay(), date.getMonth(), date.getYear());
         String dayName = DayOfWeekCalculator.getDayName(dayOfWeek);
 
-        System.out.println("День: " + day.getDay());
-        System.out.println("Месяц: " + month.getMonth());
-        System.out.println("Год: " + year.getYear());
+        System.out.println("День: " + date.getDay());
+        System.out.println("Месяц: " + date.getMonth());
+        System.out.println("Год: " + date.getYear());
         System.out.println("День недели: " + dayName);
     }
 }
